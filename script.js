0497dd62cb48ee67c973e08b7aedc663d0a08a70
AG06ag <a.george@cry.gdst.net>
2025-01-29 16:51:22 +0000

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function myFunction() {
+  document.getElementById("para1").innerHTML = "<center>The addition of concentrated milk to sweet chocolate produces milk chocolate. White chocolate, made from cocoa butter, sugar, milk, and vanilla, contains no cocoa solids. Rich in carbohydrates and fat and containing small amounts of caffeine, chocolate is an excellent source of quick energy.</center>"
+}
+function myFunction2() {
+  document.getElementById("para1").innerHTML = "In making chocolate, the kernels of fermented and roasted cacao beans are ground into a paste called chocolate liquor, which may be hardened in molds to form baking <b>(bitter)</b> chocolate, pressed to reduce the cocoa butter (vegetable fat) content and then pulverized to make cocoa pow snd teh two andn nnnbder, or mixed with sugar and additional cocoa butter to make sweet <b>(eating)</b> chocolate.";
+}
